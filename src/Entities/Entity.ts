afdd324504173d8b140cf64e5cf908471e81d2c1
Tito Alvarez <augustoalvarez@gmail.com>
2021-10-10 00:23:36 -0600

    Entity validation

--- src/Entities/Entity.ts
+++ src/Entities/Entity.ts
@@ -1,3 +1,5 @@
+import { EntityInvalidFieldTypes } from "../Errors/Entities/EntityInvalidFieldTypes";
+import { EntityRequiredFieldsNotFound } from "../Errors/Entities/EntityRequiredFieldsNotFound";
 import { IEntityDefinition } from "../Types/Entities/IEntityDefinition";
 import { FieldValidator } from "../Utilities/Fields/FieldValidator";
 import { UniqueEntityId } from "../Utilities/Ids/UniqueEntityId";
@@ -32,5 +34,68 @@ export abstract class Entity<T> {
   constructor(props: T, id?: UniqueEntityId) {
     this._id = id ? id : new UniqueEntityId();
     this.props = props;
+  }
+
+  /**
+   * Validates the entity
+   *
+   * @returns true
+   * @throws EntityRequiredFieldsNotFound|EntityInvalidFieldTypes
+   */
+  protected validate(): true {
+    // Validator for required fields
+    const requiredValidation = new FieldValidator(
+      this.definition.required,
+      this.props
+    );
+
+    // Fails if required fields are not all present using the field validator
+    const requiredFieldsValidation = requiredValidation.allFieldsAvailable();
+    if (requiredFieldsValidation !== true)
+      throw new EntityRequiredFieldsNotFound(
+        (<any>this).constructor.name,
+        requiredFieldsValidation
+      );
+
+    // Validator for optional fields
+    const optionalValidation = new FieldValidator(
+      this.definition.optional,
+      this.props
+    );
+
+    const requiredFieldsTypeValidation =
+      requiredValidation.allFieldsTypeMatch();
+    const optionalFieldsTypeValidation =
+      optionalValidation.allFieldsTypeMatch();
+
+    if (
+      requiredFieldsTypeValidation !== true ||
+      optionalFieldsTypeValidation !== true
+    ) {
+      // If the validator fails, gets a joint array of the required and optional fields that have failed
+      const invalidFields = (
+        requiredFieldsTypeValidation === true
+          ? []
+          : requiredFieldsTypeValidation
+      ).concat(
+        optionalFieldsTypeValidation === true
+          ? []
+          : optionalFieldsTypeValidation
+      );
+
+      // Throws an exception with the joint array's names and types
+      throw new EntityInvalidFieldTypes(
+        <any>this.constructor.name,
+        invalidFields.map((field) => {
+          return field.name;
+        }),
+        invalidFields.map((field) => {
+          return field.type;
+        })
+      );
+    }
+
+    // Everything fine, returns true
+    return true;
   }
 }
--- test/Entities/Entity.test.ts
+++ test/Entities/Entity.test.ts
@@ -2,6 +2,9 @@ import { Entity } from "../../src/Entities/Entity";
 import { IEntityDefinition } from "../../src/Types/Entities/IEntityDefinition";
 import { UniqueEntityId } from "../../src/Utilities/Ids/UniqueEntityId";
 import { FieldValidator } from "../../src/Utilities/Fields/FieldValidator";
+import { EntityRequiredFieldsNotFound } from "../../src/Errors/Entities/EntityRequiredFieldsNotFound";
+import { IFieldDefinition } from "../../src/Types/Entities/IFieldDefinition";
+import { EntityInvalidFieldTypes } from "../../src/Errors/Entities/EntityInvalidFieldTypes";
 
 jest.mock("../../src/Utilities/Ids/UniqueEntityId");
 jest.mock("../../src/Utilities/Fields/FieldValidator");
@@ -15,13 +18,90 @@ describe("Entity", () => {
     protected readonly definition: IEntityDefinition = {
       required: [
         {
-          name: "number",
+          name: "required",
           type: "number",
         },
       ],
-      optional: [],
+      optional: [
+        {
+          name: "optional",
+          type: "number",
+        },
+      ],
     };
+
+    public validate(): true {
+      return super.validate();
+    }
   }
+
+  beforeAll(() => {
+    function returnTrue(): true {
+      return true;
+    }
+    function returnMissingField(): string[] {
+      return ["number"];
+    }
+    function returnInvalidField(): IFieldDefinition[] {
+      return [{ name: "number", type: "number" }];
+    }
+    (FieldValidator as jest.MockedClass<any>)
+      // Validation fails because of missing required fields
+      .mockImplementationOnce(() => {
+        return {
+          allFieldsAvailable: jest.fn(returnMissingField),
+        };
+      })
+      // Validation fails because of invalid field types in both required and optional definitions - 1st pass (required fields)
+      .mockImplementationOnce(() => {
+        return {
+          allFieldsAvailable: jest.fn(returnTrue),
+          allFieldsTypeMatch: jest.fn(returnInvalidField),
+        };
+      })
+      // Validation fails because of invalid field types in both required and optional definitions - 2nd pass (optional fields)
+      .mockImplementationOnce(() => {
+        return {
+          allFieldsAvailable: jest.fn(returnTrue),
+          allFieldsTypeMatch: jest.fn(returnInvalidField),
+        };
+      })
+      // Validation fails because of invalid field types in required fields only - 1st pass (required fields)
+      .mockImplementationOnce(() => {
+        return {
+          allFieldsAvailable: jest.fn(returnTrue),
+          allFieldsTypeMatch: jest.fn(returnInvalidField),
+        };
+      })
+      // Validation fails because of invalid field types in required fields only - 2nd pass (optional fields)
+      .mockImplementationOnce(() => {
+        return {
+          allFieldsAvailable: jest.fn(returnTrue),
+          allFieldsTypeMatch: jest.fn(returnTrue),
+        };
+      })
+      // Validation fails because of invalid field types in optional fields only - 1st pass (required fields)
+      .mockImplementationOnce(() => {
+        return {
+          allFieldsAvailable: jest.fn(returnTrue),
+          allFieldsTypeMatch: jest.fn(returnTrue),
+        };
+      })
+      // Validation fails because of invalid field types in optional fields only - 2nd pass (optional fields)
+      .mockImplementationOnce(() => {
+        return {
+          allFieldsAvailable: jest.fn(returnTrue),
+          allFieldsTypeMatch: jest.fn(returnInvalidField),
+        };
+      })
+      // Validation succeeds
+      .mockImplementation(() => {
+        return {
+          allFieldsAvailable: jest.fn(returnTrue),
+          allFieldsTypeMatch: jest.fn(returnTrue),
+        };
+      });
+  });
 
   test("Entity can be successfully instantiated without id", () => {
     const e = new MockEntity(mockProps);
@@ -31,5 +111,50 @@ describe("Entity", () => {
   test("Entity can be successfully instantiated with id", () => {
     const e = new MockEntity(mockProps, new UniqueEntityId());
     expect(e).toBeInstanceOf(Entity);
+  });
+
+  test("Validation fails because of missing required fields", () => {
+    const e = new MockEntity(mockProps);
+    try {
+      e.validate();
+      throw new Error();
+    } catch (err) {
+      expect(err).toBeInstanceOf(EntityRequiredFieldsNotFound);
+    }
+  });
+
+  test("Validation fails because of invalid field types in both required and optional definitions", () => {
+    const e = new MockEntity(mockProps);
+    try {
+      e.validate();
+      throw new Error();
+    } catch (err) {
+      expect(err).toBeInstanceOf(EntityInvalidFieldTypes);
+    }
+  });
+
+  test("Validation fails because of invalid field types in required fields only", () => {
+    const e = new MockEntity(mockProps);
+    try {
+      e.validate();
+      throw new Error();
+    } catch (err) {
+      expect(err).toBeInstanceOf(EntityInvalidFieldTypes);
+    }
+  });
+
+  test("Validation fails because of invalid field types in optional fields only", () => {
+    const e = new MockEntity(mockProps);
+    try {
+      e.validate();
+      throw new Error();
+    } catch (err) {
+      expect(err).toBeInstanceOf(EntityInvalidFieldTypes);
+    }
+  });
+
+  test("Validation succeeds", () => {
+    const e = new MockEntity(mockProps);
+    expect(e.validate()).toBe(true);
   });
 });
