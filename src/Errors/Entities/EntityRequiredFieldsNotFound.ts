58db37b6da0162d78f4c0b52aa9088764b9a2c26
Tito Alvarez <augustoalvarez@gmail.com>
2021-10-09 21:37:42 -0600

    Entity exceptions with more detail about the problem

---
+++ src/Errors/Entities/EntityRequiredFieldsNotFound.ts
@@ -0,0 +1,14 @@
+import { NotFoundError } from "../NotFoundError";
+
+export class EntityRequiredFieldsNotFound extends NotFoundError {
+  message: string = "Required field(s) %s was not found in %s";
+
+  constructor(entity: string, fields: string[]) {
+    super();
+    const plural = fields.length > 1 ? "s" : "";
+    const verb = fields.length > 1 ? "were" : "was";
+    const fieldList = fields.join(", ");
+    this.message = `Required field${plural} ${fieldList} ${verb} not found in ${entity}`;
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+}
